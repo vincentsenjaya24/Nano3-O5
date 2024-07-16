14ecb1b7644b5f8a091fdb87885dfe2f46d0c2e8
Reza Athallah <rezaa.ath@gmail.com>
2024-07-16 14:32:14 +0700

    calculator services and view model for view

--- Weightlifting-Calculator/ViewModels/ContentViewModel.swift
+++ Weightlifting-Calculator/ViewModels/ContentViewModel.swift
@@ -8,6 +8,21 @@
 import Foundation
 
 class ContentViewModel: ObservableObject {
+    var calculatorService: CalculatorProtocol
     
+    @Published var currentRM = 0.0
+    @Published var currentWeight = 0.0
+    @Published var currentReps = 0
     
+    init(calculator: CalculatorProtocol = CalculatorService()) {
+        self.calculatorService = calculator
+    }
+    
+    func calculateRM() {
+        currentRM = calculatorService.getRM(weight: currentWeight, reps: currentReps)
+    }
+    
+    func setRM(rm: Double) {
+        currentRM = rm
+    }
 }
